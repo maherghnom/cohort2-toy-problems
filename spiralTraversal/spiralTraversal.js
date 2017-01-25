c54a461a0799f7d0c62873971fc0ef605050c8b2
Admin <m2ghanom@gmail.com>
2017-01-25 09:25:59 +0200

    :

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -9,7 +9,43 @@
     ]);
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
-
+///// time complexity  quadratic
 var spiralTraversal = function(matrix){
+  var result=[];
+  var i=0;
+  var k=0;
+  for (var j=0;j<Math.pow(matrix.length,2 );j++){
+      result.push(matrix[i][k])
+      if(i===k&&k!==0){
+        for(var o=0;o<Math.pow(matrix.length,2 );o++){
+          //k=k-1;
+          result.push(matrix[i][--k]);
+
+          if(k===0){
+            i=matrix.length- (matrix.length-1)
+            
+
+            for(var w=0;w<Math.pow(matrix.length,2 );w++){
+              result.push(matrix[i][k])
+              if(k===matrix.length-2){
+                return result
+              }
+              k++;
+            }
+          }
+        }
+      }
+      if(k===matrix.length-1){
+        i++;
+        k=matrix.length-2
+      } 
+      if(i===matrix.length-1){
+        i=matrix.length-1;
+        k=matrix.length-2
+      }
+
+
+      k++;
+  }
 
 };
